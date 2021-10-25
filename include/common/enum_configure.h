3aa33d854e1cb135f1d49e8ee290be6972e41be2
st9540808 <st9540808@gmail.com>
2021-10-26 02:04:05 +0800

    [WASI] Add function templates for wasi-nn
    This is my first commit to add function templates for implementing WASI-NN.
    wasi-nn-example.(wasm|wat) is used to verify whether host functions are
    correctly called or not.
    
    Signed-off-by: st9540808 <st9540808@gmail.com>

--- include/common/enum_configure.h
+++ include/common/enum_configure.h
@@ -96,7 +96,12 @@ enum WasmEdge_Proposal {
 namespace WasmEdge {
 
 /// Host Module Registration C++ enumeration class.
-enum class HostRegistration : uint8_t { Wasi = 0, WasmEdge_Process, Max };
+enum class HostRegistration : uint8_t {
+  Wasi = 0,
+  WasmEdge_Process,
+  WasiNN,
+  Max
+};
 
 } // namespace WasmEdge
 #endif
